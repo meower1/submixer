40a5dfa2a1f64bb7d08129d68a6db0c40f053366
meower1 <109747197+meower1@users.noreply.github.com>
2023-09-09 13:26:32 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import base64
+import time
+
+def main():
+
+    while True:
+        with open('urls.txt', 'r') as file:
+            file_content = file.read()
+            
+        urls = file_content.split("\n")
+
+        links = ""
+        temp = ""
+
+        for i in urls:
+            response = requests.get(i)
+            try:
+                temp = base64.b64decode(response.text).decode("utf-8")
+            except:
+                links += response.text
+            links += temp
+
+
+        with open('Flask/templates/goober.html', 'w') as goober:
+            goober.write(links)
+            goober.close()
+
+        time.sleep(300)
+
+main()
