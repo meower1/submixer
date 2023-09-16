125e35b1094c768dd9e3c305ac85fc07f8c160e6
meower1 <109747197+meower1@users.noreply.github.com>
2023-09-16 15:09:03 +0330

    update

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from waitress import serve
+from os import path, walk
+
 
 # creates a Flask application
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/")
@@ -11,6 +14,7 @@ def hello():
 	return render_template('goober.html',
 						message=message)
 
+
 # run the application
 if __name__ == "__main__":
-	app.run(debug=True)
+	serve(app, host='0.0.0.0', port=5020, threads=1, url_prefix='/sub', url_scheme='https')
